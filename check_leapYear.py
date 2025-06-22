6af2167b2608c6ca79ab779fae2790995d58f7ce
Krish <krishantamunda@gmail.com>
2025-06-22 11:47:04 +0530

    Day 1 codes

--- check_leapYear.py
+++ check_leapYear.py
@@ -1,4 +1,6 @@
 #1.Basic If-Else Method
+#Time Complexity: O(1) — Only 3 modulus and a few logical operations.
+#Space Complexity: O(1) — One integer variable year.
 year = int(input("Enter a year:"))
 
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
@@ -8,6 +10,8 @@ else:
 
 
 #2.Using Function
+# Time Complexity: O(1) — Same logic as above.
+# Space Complexity: O(1) — Extra for function call (but constant).
 def is_leap(year):
     if (year % 4 == 0 and year %100 !=0)or (year % 400 == 0):
         print(f"{year} is a leap year.")
@@ -19,6 +23,8 @@ is_leap(2024)
 is_leap(1900)
 
 #3.Using Nested If statements 
+# Time Complexity: O(1) — At most 3 modulus and comparisons.
+# Space Complexity: O(1) — Same.
 year = int(input("Enter a year:"))
 
 if year % 4 == 0:
@@ -32,7 +38,10 @@ if year % 4 == 0:
 else:
     print(f"{year} is not a leap year.")
 
+
 #4.Using python's calender Module
+# Time Complexity: O(1) — Internally uses same logic.
+# Space Complexity: O(1) — Only loads a lightweight module, no extra data used.
 import calendar
 
 year = int(input("Enter a year: "))
@@ -41,7 +50,10 @@ if calendar.isleap(year):
 else:
     print(f"{year} is not a leap year.")
 
+
 #5.Leap year for a Range of Years
+# Time Complexity: O(n) — where n = end - start + 1
+# Space Complexity: O(1) — Only one loop variable; no extra memory used.
 start_year = int(input("Enter start year: "))
 end_year = int(input("Enter end year: "))
 
